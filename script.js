747fd14ac4aad172ea2c9a682b89485431840ace
Cecios <andrea.raneri94@gmail.com>
2024-01-08 22:42:58 +0100

    committ search + cards

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-const url_eminem = 'http://striveschool-api.herokuapp.com/api/deezer/search?q=eminem';
-const url_metallica = 'http://striveschool-api.herokuapp.com/api/deezer/search?q=metallica';
-const url_queen = 'http://striveschool-api.herokuapp.com/api/deezer/search?q=queen';
 
-const fetchEminem = () => {
-    fetch(url_eminem, {method: "GET",
+const fetchGruppo = (input) => {
+    fetch(`http://striveschool-api.herokuapp.com/api/deezer/search?q=${input}`, {method: "GET",
 }).then((response)=>{
     //console.log(response);
     return response.json();
 }).then((data)=>{
     console.log(data.data);
     const brani = data.data
-    const eminem = document.querySelector('#eminemSection')
-    console.log(brani);
+    const gruppo = document.querySelector('#gruppoSection')
+    const titoloGruppo = document.querySelector('#titoloGruppo')
+    gruppo.innerHTML = ''
 for (let i = 0; i < brani.length; i++) {
-    eminem.innerHTML += `<div class="col-3">
-                           <img src="${brani[i].artist.picture_big}" alt="">
-                           <h5>${brani[i].title}</h5>
-                         </div>
+    gruppo.innerHTML += `                  <div class="card bg-transparent">
+    <img src="${brani[i].artist.picture_big}" class="card-img-top w-100" alt="">
+    <div class="card-body">
+      <h4>${brani[i].title}</h4>
+      <h6>Durata:${brani[i].duration} </h6>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+    <div class="card-footer bg-transparent border-success">
+
+      <audio class="w-100" controls>
+        <source src="${brani[i].preview}" type="audio/mp3">
+        
+      </audio>
+    </div>
+  </div>
     `
+    titoloGruppo.innerHTML = `${brani[i].artist.name}`
 }
-const eminemClass = document.querySelector('#eminem')
-console.log(eminemClass);
-eminemClass.classList.toggle('d-none')
+const gruppoClass = document.querySelector('#gruppo')
+gruppoClass.classList.remove('d-none')
 })
 
 }
 //fetchEminem();
 const creaLista = document.querySelector('#creaLista')
-creaLista.onclick = fetchEminem
+
+
+const search = () => {
+
+    let input = document.querySelector('#searchField')
+    console.log(input.value);
+    input = input.value
+    fetchGruppo(input)
+}
